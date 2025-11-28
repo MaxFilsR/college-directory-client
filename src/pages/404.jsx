bb51fc3e0a5d3ec9be2b2ecfab63f37db8612f29
MaxFilsR <maxfilsr@gmail.com>
2025-11-28 15:34:26 -0500

    switch to using cusotm color pallet

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -8,12 +8,12 @@ const PageNotFound = () => {
         backgroundImage: `url("https://picsum.photos/1600/900?random=${Math.random()}")`,
       }}
     >
-      <div className=" bg-cover bg-black h-full opacity-70 bg-center text-green-200 flex flex-col justify-center items-center gap-4">
+      <div className=" bg-cover bg-background h-full opacity-70 bg-center text-primary flex flex-col justify-center items-center gap-4">
         <h1 className="text-9xl font-bold">404</h1>
         <h2 className="text-3xl">Looks like you got lost</h2>
         <Link
           to={"/"}
-          className="cursor-pointer bg-green-200 p-2 rounded opacity-100! text-black hover:bg-green-500"
+          className="cursor-pointer bg-primary p-2 rounded opacity-100! text-black hover:bg-accent"
         >
           Back To Home
         </Link>
